d03ce0aad93f458a59c04ff3cb8bb0724cf0e8e8
Eric Tobin <eatobin@gmail.com>
2018-02-12 22:01:42 -0800

    to 3.2

---
+++ hour03/example01/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s = "Hi world!"
+	fmt.Println(s)
+}
---
+++ hour03/example02/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string
+	s = "Bye world"
+	var a = s == "Bye world"
+	fmt.Println(s)
+	fmt.Println(a)
+}
