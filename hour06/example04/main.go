8bc6b86f8ae5b365cd8b0a3099740c10389a0be3
Eric Tobin <eatobin@gmail.com>
2018-02-19 08:54:46 -0800

    6.4++

--- hour06/example04/main.go
+++ hour06/example04/main.go
@@ -8,10 +8,14 @@ func main() {
 	cheeses[1] = "Yellow"
 	cheeses[2] = "White"
 	fmt.Println(len(cheeses))
+	fmt.Println(cap(cheeses))
 	fmt.Println(cheeses)
 	var cheeses2 = append(cheeses[:0], cheeses[:1]...)
 	fmt.Println(len(cheeses2))
+	fmt.Println(cap(cheeses2))
 	fmt.Println(cheeses2)
+	fmt.Println(cheeses2[0])
+	fmt.Println(cheeses2[0:3])
 	var cheeses3 = append(cheeses[:1], cheeses[1+1:]...)
 	fmt.Println(len(cheeses3))
 	fmt.Println(cheeses3)
@@ -30,4 +34,16 @@ func main() {
 	fmt.Println(y[0])
 	fmt.Println(y[1])
 	fmt.Println(y[0:4])
+
+	fmt.Println()
+
+	a := []int{0,1,2,3,4}
+	fmt.Println(a,len(a),cap(a))
+
+	b := append(a[:1],a[1+1:]...)
+	fmt.Println(b,len(b),cap(b))
+	fmt.Println(a,len(a),cap(a))
+	fmt.Println(a[0:4])
+	fmt.Println(a[0:5])
+	fmt.Println(b[0:5])
 }
