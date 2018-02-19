d389745cd9d2ebe12e31151de3b380aa55da621c
Eric Tobin <eatobin@gmail.com>
2018-02-19 08:17:54 -0800

    6.4++

--- hour06/example04/main.go
+++ hour06/example04/main.go
@@ -18,4 +18,16 @@ func main() {
 	var cheeses4 = append(cheeses[:0], cheeses[2:]...)
 	fmt.Println(len(cheeses4))
 	fmt.Println(cheeses4)
+
+	x := []int{2,3,5,7,11}
+	fmt.Println(x)
+	fmt.Println(x[:1])
+	fmt.Println(x[0:1])
+	fmt.Println(x[4:])
+
+	y := x[1:3]
+	fmt.Println(y)
+	fmt.Println(y[0])
+	fmt.Println(y[1])
+	fmt.Println(y[0:4])
 }
