f1e8fdbfd6edbcc6b9bc2fa83ff8aebe9e5e7369
Eric Tobin <eatobin@gmail.com>
2018-02-25 18:35:03 -0600

    corrections to hr6

--- hour05/example04.go
+++ hour05/example04.go
@@ -6,7 +6,9 @@ func main() {
 	i := 2
 	if i == 3 {
 		fmt.Println("i is 3")
-	} else if i == 2 {
-		fmt.Println("i is 2")
+	} else if i == 9 {
+		fmt.Println("i is 9")
+	} else {
+		fmt.Print("i is 2!")
 	}
 }
