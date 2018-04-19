69535ac1fc364170413ae0b7c5b9950cd3104f21
Eric Tobin <eatobin@gmail.com>
2018-04-19 11:44:34 -0500

    nameX!

--- hour25/example02/main.go
+++ hour25/example02/main.go
@@ -8,18 +8,18 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+func Index(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome stranger!\n")
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+func Hello(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("nameX"))
 }
 
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
+	router.GET("/hello/:nameX", Hello)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
